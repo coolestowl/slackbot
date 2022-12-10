839170ebb9d2e0dc77f0ae3c9ac1b8a009d5ef8a
owl <97215481+coolestowl@users.noreply.github.com>
2022-12-10 23:45:02 +0800

    feat(app): add event api

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 #[macro_use] extern crate rocket;
 
-use rocket::serde::{Serialize, json::Json};
+use rocket::serde::{Serialize, Deserialize, json::Json};
 
 #[get("/")]
 fn index() -> &'static str {
@@ -8,25 +8,51 @@ fn index() -> &'static str {
 }
 
 #[derive(Serialize)]
-struct Resp<'r> {
-    msg: &'r str,
+struct Resp {
+    msg: String,
+    chanllenge: String,
 }
 
 #[post("/interactivity", data = "<data>")]
-fn interactivity(data: String) -> Json<Resp<'static>> {
+fn interactivity(data: String) -> Json<Resp> {
     println!("rcv: {}", data);
-    Json(Resp { msg: "ok" })
+    Json(Resp {
+        msg: "ok".into(),
+        chanllenge: "".into(),
+    })
+}
+
+#[derive(Deserialize, Debug)]
+struct ChanllengeReq {
+    token: String,
+    chanllenge: String,
+    tp: String,
+}
+
+#[post("/event", data = "<req>")]
+fn event(req: Json<ChanllengeReq>) -> Json<Resp> {
+    println!("evt: {:?}", req);
+
+    let data = req.chanllenge.clone();
+
+    Json(Resp {
+        msg: "ok".into(),
+        chanllenge: data,
+    })
 }
 
 #[post("/slash/vul", data = "<data>")]
-fn vul(data: String) -> Json<Resp<'static>> {
+fn vul(data: String) -> Json<Resp> {
     println!("vul: {}", data);
-    Json(Resp { msg: "ok" })
+    Json(Resp {
+        msg: "ok".into(),
+        chanllenge: "".into(),
+    })
 }
 
 #[launch]
 fn rocket() -> _ {
     rocket::build()
         .mount("/", routes![index])
-        .mount("/api/v1", routes![interactivity, vul])
+        .mount("/api/v1", routes![interactivity, vul, event])
 }
